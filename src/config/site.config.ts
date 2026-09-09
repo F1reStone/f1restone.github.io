0b05f212c7ea452d9dc0154a7020eb9b4b3041e9
FireStone <fsfirestone@outlook.com>
2026-09-10 00:32:21 +0800

    Let the TOC back to right (formally left) and let project tag use TagList Component

--- src/config/site.config.ts
+++ src/config/site.config.ts
@@ -293,7 +293,7 @@ const siteConfig: SiteConfig = {
     toc: {
       enabled: true,
       layout: 'auto',
-      sidebarPosition: 'left',
+      sidebarPosition: 'right',
       minHeadings: 3,
       maxDepth: 3,
     },
